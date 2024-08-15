24df753b7ec6a94b6ac28ebf31fcfb117b49ffb9
zaynab <zeinabmohammed817@gmail.com>
2024-08-15 05:57:11 +0300

    EC2 instance

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-07c8c1b18ca66bb07"
+}
+
+variable "public_key" {
+  description = "ssh public key"
+}
