b055673524f0aba0fe09b272bd13e1e8fa23d97d
zaynab <zeinabmohammed817@gmail.com>
2024-08-19 06:53:28 +0300

    enable jenkins to use k8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ubuntu-instance" {
   ami           = var.ami
-  instance_type = "t3.micro"
+  instance_type = "t3.meduim"
   key_name      = "EC2_Key"
   security_groups = ["${aws_security_group.UbuntuSG.name}"]
 
